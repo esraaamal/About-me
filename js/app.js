2e05c3f816270e887a1abf38c641d05ebe8abf09
esraaamal <ma.esraa@hotmail.com>
2020-02-18 19:33:42 +0200

    rff

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
+
 var names =prompt("what is your name please ");
 document.write("Good morning"+' '+names);
 
 
+var score=0;
 
 
  var answerA = prompt("do you think that i can pass the exam ?").toLowerCase();
@@ -11,6 +13,7 @@ document.write("Good morning"+' '+names);
      answerA = prompt("do you think that i can pass the exam? please do yes or no answer").toLowerCase();}
     console.log(answerA);
     if(answerA === "yes" || answerA === "y"){
+        score=score+1;
         alert("yes..that's right !  ");
     }else if(answerA === "no" || answerA==="n"){
         alert("no that's wrong ");
@@ -30,6 +33,7 @@ document.write("Good morning"+' '+names);
      answerB = prompt("do you think i love banana ? please do yes or no answer").toLowerCase();}
     console.log(answerB);
     if(answerB === "yes" || answerB === "y"){
+        score=score+1;
         alert("yes..that's right !  ");
     }else if(answerB === "no" || answerB ==="n"){
         alert("no that's wrong ");
@@ -47,7 +51,9 @@ document.write("Good morning"+' '+names);
      answerc = prompt("i am working or not ? please do yes or no answer").toLowerCase();}
     console.log(answerc);
     if(answerc === "yes" || answerc === "y"){
+        score=score+1;
         alert("yes..that's right !  ");
+        
     }else if(answerc === "no" || answerc ==="n"){
         alert("no that's wrong ");
     }
@@ -61,7 +67,9 @@ while((answerD !== 'yes'&& answerD !== 'y') && (answerD !== 'no'&& answerD !== '
 console.log(answerD);
 console.log((answerD === "yes" || answerD === "y"));
 if(answerD === "yes" || answerD === "y"){
+    score=score+1;
     alert("yes..that's right !  ");
+    
 }else if(answerD === "no" || answerD ==="n"){
     alert("no that's wrong ");
 }
@@ -69,13 +77,106 @@ if(answerD === "yes" || answerD === "y"){
 
 
 var answerf = prompt("do you think i love coding ?").toLowerCase();
- while((answerf !== 'yes'&& answerf!== 'y') && (answerf !== 'no'&& answerf !== 'n') ){
-     answerA = prompt("do you think i love coding ? answer with yes or no please").toLowerCase();}
-    console.log(answerf);
-    if(answerf === 'yes' || answerf === 'y'){
-        alert("yes..that's right !  ");
-    }else if(answerf === "no" || answerf ==="n"){
-        alert("no that's wrong ");
-    }
+while((answerf !== 'yes'&& answerf !== 'y') && (answerf !== 'no'&& answerf !== 'n') ){
+   answerD = prompt("do you think i love coding ? please answer with yes or no").toLowerCase();}
 
+console.log(answerf);
+console.log((answerf === "yes" || answerf === "y"));
+if(answerf === "yes" || answerf === "y"){
+    score=score+1;
+    alert("yes..that's right !  ");
+    
+}else if(answerf === "no" || answerf ==="n"){
+    alert("no that's wrong ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+var myAge = prompt("can you know how old i'm ?");
+    myAge=Number(myAge)
+    if(myAge > 0 && myAge <= 10){
+        alert("No do i look so young ...try again ");
+        for(var k=0 ; k<3 ;k++){
+            myAge = prompt("can you know how old i'm ?");
+        }
+    }else if(myAge === 22){
+        score=score+1;
+        alert("oh my God that's totaly right ..you are Gorgeous ");
+    }
+    else if(myAge > 20  &&  myAge <= 30){
+
+        alert("you are close to answering ...try again please ");
+        for(var k=0 ; k<3 ;k++){
+            myAge = prompt("can you know how old i'm ?");
+        }
+
+    }
+    else if(myAge > 10 && myAge <= 20){
+
+        alert("no that not true ...try again please ");
+        for(var k=0 ; k<3 ;k++){
+            myAge = prompt("can you know how old i'm ?");
+        }
+        
+    }else if(myAge > 30 && myAge <= 1000){
+        alert("no that not true ...try again please ");
+        for(var k=0 ; k<3 ;k++){
+            myAge = prompt("can you know how old i'm ?");
+        }
+
+        
+    }
+    alert("my age is 22");
+    
+
+
+
+   var myColor=['green' , 'pink'];
+
+   var answerseven = prompt("tell me one of my favorit colors ?").toLowerCase();
+   for(var j=0 ; j<=myColor.length ; j++){
+   
+       if (answerseven === myColor[j] ){
+           score=score+1;
+           alert("cool..this is my favorit color");
+           break;
+       }else{ 
+           
+           for(var f=0 ;f<5 ;f++){
+               alert("no that not true ...try again please ");
+           answerseven = prompt("tell me one of my favorit colors ?").toLowerCase();
+   
+           }
+           break;
+   
+       }
+   
+   }
+   alert( "my favorit colors " +' :'+ myColor);
+   alert ("your degree is: " + score +" out of 7");
+   
+
+
+    
+
+
+
+
+
+
+    
+
+
+
+    
 

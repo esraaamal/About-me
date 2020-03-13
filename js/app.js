cb70870c91ad98c16d789b827cc90d2cab031485
esraaamal <ma.esraa@hotmail.com>
2020-03-13 18:21:29 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ var qOne = function(){
     }}
  
     var qFour = function(){
- var answerD = prompt("do you think i'am a happy person? ?").toLowerCase();
+ var answerD = prompt("do you think i'am a happy person? ?").toUpperCase;
 while((answerD !== 'yes'&& answerD !== 'y') && (answerD !== 'no'&& answerD !== 'n') ){
    answerD = prompt("do you think i'am a happy person? please answer with yes or no").toLowerCase();}
 //console.log(answerD);
@@ -73,70 +73,75 @@ if(answerf === "yes" || answerf === "y"){
 }}
  
     var qSex = function(){
+        for(var i=0; i<4 ;i++){
 var myAge = prompt("can you know how old i'm ?");
 //console.log(myAge);
-    myAge=Number(myAge)
+    myAge=Number(myAge);
     if(myAge > 0 && myAge <= 10){
         alert("No do i look so young ...try again ");
-        for(var k=0 ; k<3 ;k++){
-            myAge = prompt("can you know how old i'm ?");
-        }
-    }else if(myAge === 22){
-        score=score+1;
+        // for(var k=0 ; k<3 ;k++){
+        //     myAge = prompt("can you know how old i'm ?");
+        // }
+    }else if(myAge == 22){
+        // score=score+1;
         alert("oh my God that's totaly right ..you are Gorgeous ");
+        break;
     }
     else if(myAge > 20  &&  myAge <= 30){
         alert("you are close to answering ...try again please ");
-        for(var k=0 ; k<3 ;k++){
-            myAge = prompt("can you know how old i'm ?");
-        }
+        // for(var k=0 ; k<3 ;k++){
+        //     myAge = prompt("can you know how old i'm ?");
+        // }
     }
     else if(myAge > 10 && myAge <= 20){
         alert("no that not true ...try again please ");
-        for(var k=0 ; k<3 ;k++){
-            myAge = prompt("can you know how old i'm ?");
-        }
+        // for(var k=0 ; k<3 ;k++){
+        //     myAge = prompt("can you know how old i'm ?");
+        // }
         
     }else if(myAge > 30 && myAge <= 1000){
         alert("no that not true ...try again please ");
-        for(var k=0 ; k<3 ;k++){
-            myAge = prompt("can you know how old i'm ?");
-        }
-        
+        // for(var k=0 ; k<3 ;k++){
+        //     myAge = prompt("can you know how old i'm ?");
+        // }
+    }
     }
     alert("my age is 22");}
     
 
 
-    var qSeven = function(){
-   var myColor=['green' , 'pink' ,'blue' ,'black'];
+var myColor=['green' , 'pink' ,'blue' ,'black'];
 
-   var answerseven = prompt("tell me one of my favorit colors ?").toLowerCase();
+    var qSeven = function(){
+   
    console.log(answerseven)
+   for(var i=0 ;i<6 ;i++){
+    var answerseven = prompt("tell me one of my favorit colors ?").toLowerCase();
    for(var j=0 ; j<= myColor.length ; j++){
    
        if (answerseven === myColor[j] ){
-           score=score+1;
+        //    score=score+1;
            alert("cool..this is my favorit color");
-           break;
+           i=6; 
+        break;}
+           
+          
        }
+      if (answerseven != myColor[j] ){
+           alert("no that not true ...try again please ");
+          
        }
-   for(var l=0 ; l<5 ;l++){
-       if (answerseven !== myColor[j] )
-       {
-        alert("no that not true ...try again please ");
-        answerseven = prompt("tell me one of my favorit colors ?").toLowerCase(); 
-       }
-}
+
+//  
+    }
+    alert( "my favorit colors : " +' :'+ myColor);
+    }
+   
+//    alert (names + " your degree is: " + score +" out of 7");
+//    console.log("my error is :" + score)
 
 
-   alert( "my favorit colors : " +' :'+ myColor);
-   alert (names + " your degree is: " + score +" out of 7");
-   console.log("my error is :" + score)
-
-   alert( "my favorit colors : " +' :'+ myColor);}
-
-   // call function
+//    call function
    qOne();
    qTwo();
    qThree();
@@ -146,3 +151,12 @@ var myAge = prompt("can you know how old i'm ?");
    qSeven();
    alert (names + " your degree is: " + score +" out of 7");
 
+
+
+// for(var l=0 ; l<5 ;l++){
+    //        if (answerseven !== myColor[j] )
+    //        {
+    //         alert("no that not true ...try again please ");
+    //         answerseven = prompt("tell me one of my favorit colors ?").toLowerCase(); 
+    //        }
+    // }
